a0587b5cf8dd174c96ef3899ee5a2f243c3cebf5
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-02 10:49:59 +0100

    escape fields

--- src/Routes/Query.php
+++ src/Routes/Query.php
@@ -83,7 +83,7 @@ group by
     wahlschein.stimmzettel";
 
     public static function prepareQuerySQL($db){
-        $fld = $db->singleValue('select group_concat(concat(table_name,".",column_name) separator ",") fld from ds_column where table_name="wahlberechtigte_anlage"
+        $fld = $db->singleValue('select group_concat(concat(table_name,".`",column_name,"`") separator ",") fld from ds_column where table_name="wahlberechtigte_anlage"
          and column_name not in (
                 "stimmzettel",
                 "wahlscheinnnummer",
