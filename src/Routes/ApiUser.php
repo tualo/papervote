b9035e9b4800d7edd5eab0b0aa179da81d46dc68
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-14 09:44:42 +0100

    fix composer

--- src/Routes/ApiUser.php
+++ src/Routes/ApiUser.php
@@ -4,6 +4,8 @@ namespace Tualo\Office\PaperVote\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
+use Tualo\Office\TualoPGP\TualoApplicationPGP;
+
 use Ramsey\Uuid\Uuid;
 
 class ApiUser implements IRoute{
