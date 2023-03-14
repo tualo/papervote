492e774480968a77619963f00a8971cec0c6422d
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-14 09:39:45 +0100

    testing generate token

--- src/Routes/ApiUser.php
+++ src/Routes/ApiUser.php
@@ -4,16 +4,54 @@ namespace Tualo\Office\PaperVote\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
+use Ramsey\Uuid\Uuid;
 
 class ApiUser implements IRoute{
  
     public static function register(){
-        BasicRoute::add('/papervote/wmregister/(?P<username>[\w\-\_\d]+)',function($matches){
+        BasicRoute::add('/papervote/get/(?P<username>[\w\-\_\d]+)',function($matches){
             try{
-                $sql='select * from view_voters_by_username_api where username={username}';
                 $db = App::get('session')->getDB();
+                $keyData = $db->singleRow("select publickey from wm_pgp_pubkey where id=0 ",[]);
+
+                $sql='select * from view_voters_by_username_api where username={username}';
                 App::contenttype('application/json');
                 $data = $db->singleRow($sql,$matches);
+
+                if ($data !== false){
+                    if (is_string($data['possible_ballotpapers'])){
+                        $data['possible_ballotpapers'] = json_decode($data['possible_ballotpapers'],true);
+                    }
+
+                    
+                    foreach( $data['possible_ballotpapers'] as &$ballotpaper){
+                        if ($ballotpaper['canvote']==1){
+                            $hash = [
+                                'voter_id'=>$ballotpaper['voter_id'],
+                                'ballotpaper_ridx' => $ballotpaper['ballotpaper_id'],
+                                'secret_token' => Uuid::uuid4()->toString()
+                            ];
+
+                            $db->direct('
+                            insert into wm_wahlschein_register (
+                                id,
+                                stimmzettel,
+                                createdate,
+                                token
+                            ) values (
+                                {voter_id},
+                                {ballotpaper_ridx},
+                                now(),
+                                {secret_token}
+                            ) on duplicate key update token=values(token)
+                            ',$hash);
+    
+                        }
+
+                        $ballotpaper['secret_token'] = TualoApplicationPGP::encrypt($keyData['publickey'],$hash['secret_token']);
+                    }
+                }
+
                 App::result('data',  $data );
                 App::result('success',$data !== false);
                 App::result('msg',($data === false)?'Der Wähler wurde nicht gefunden.':'');
@@ -25,5 +63,6 @@ class ApiUser implements IRoute{
         },['get','post'],true);
 
 
+        
     }
 }
