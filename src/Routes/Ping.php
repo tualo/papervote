d0935de5f3384c4b5f03b18bc590667c8b9e5ea5
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-15 12:58:15 +0100

    add signature ping

--- src/Routes/Ping.php
+++ src/Routes/Ping.php
@@ -14,7 +14,27 @@ class Ping implements IRoute{
     public static function register(){
         BasicRoute::add('/papervote/ping',function($matches){
             App::contenttype('application/json');
-            App::result('success',true);
+            
+            try{
+
+               if (
+                    isset($_REQUEST['signature']) &&
+                    isset($_REQUEST['message'])
+                ){
+                    $db = App::get('session')->getDB();
+                    $privatePEM = $db->singleValue("select property FROM system_settings WHERE system_settings_id = 'erp/privatekey'",[],'property');
+                    RSA::load($privatePEM)->getPublicKey()->verify($message, $signature);
+                    App::result('success',true);
+                    App::result('signature_ok',true);
+                }else{
+                    App::result('success',true);
+                }
+
+            }catch(\Exception $e){
+
+                App::result('last_sql', $db->last_sql);
+                App::result('msg', $e->getMessage());
+            }
         },['get','post'],true);
 
 
