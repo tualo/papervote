23e2eed1c3ef03bbeecc49bd728c553231f1a131
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-27 14:57:31 +0200

    fixes

--- src/sql/view_voters_by_username_api.sql
+++ src/sql/view_voters_by_username_api.sql
@@ -1,22 +1,41 @@
+
+select 
+concat('JSON_OBJECT(',group_concat( concat('"',column_name,'",wahlberechtigte_anlage.',column_name ) separator ',') ,')') s
+from ds_column where table_name='wahlberechtigte_anlage' and existsreal=1
+
 CREATE OR REPLACE VIEW `view_voters_by_username_api` AS
 select
     `q`.`username` AS `username`,
     `q`.`pwhash` AS `pwhash`,
     `q`.`id` AS `id`,
-    -- `w`.`token` AS `secret_token`,
     concat(
         '[',
         group_concat(
             json_object(
-                'state', `wahlschein`.`wahlscheinstatus`,
-                'voter_id', `wahlschein`.`id`,
-                'ballotpaper_id', `stimmzettel`.`id`,
+                'state',
+                `bwkiel`.`wahlschein`.`wahlscheinstatus`,
+                'voter_id',
+                `bwkiel`.`wahlschein`.`id`,
+                'ballotpaper_id',
+                `bwkiel`.`stimmzettel`.`id`,
                 'canvote',
                 if(
-                    `wahlschein`.`wahlscheinstatus` in (select wahlscheinstatus from wahlscheinstatus_online_erlaubt),
+                    `bwkiel`.`wahlschein`.`wahlscheinstatus` in (
+                        select
+                            `bwkiel`.`wahlscheinstatus_online_erlaubt`.`wahlscheinstatus`
+                        from
+                            `bwkiel`.`wahlscheinstatus_online_erlaubt`
+                    ),
                     1,
                     0
-                )
+                ),
+                
+                'ballotpaper_name', `bwkiel`.`stimmzettel`.name,
+
+                'voter_data',
+                    JSON_OBJECT("briefanrede",wahlberechtigte_anlage.briefanrede,"identnummer",wahlberechtigte_anlage.identnummer,"kombiniert",wahlberechtigte_anlage.kombiniert,"post_name0",wahlberechtigte_anlage.post_name0,"post_name1",wahlberechtigte_anlage.post_name1,"post_name2",wahlberechtigte_anlage.post_name2,"post_name3",wahlberechtigte_anlage.post_name3,"post_name4",wahlberechtigte_anlage.post_name4,"pwhash",wahlberechtigte_anlage.pwhash,"stimmzettel",wahlberechtigte_anlage.stimmzettel,"username",wahlberechtigte_anlage.username,"wahlb",wahlberechtigte_anlage.wahlb,"wahlg",wahlberechtigte_anlage.wahlg,"wahlscheinnummer",wahlberechtigte_anlage.wahlscheinnummer)
+
+
             ) separator ','
         ),
         ']'
@@ -24,21 +43,28 @@ select
 from
     (
         (
-            `wahlschein`
-            join `stimmzettel` on `wahlschein`.`stimmzettel`=`stimmzettel`.`ridx`
-            join (
-                select
-                    `wahlschein`.`id` AS `id`,
-                    `wahlschein`.`pwhash` AS `pwhash`,
-                    `wahlschein`.`username` AS `username`,
-                    `wahlschein`.`kombiniert` AS `kombiniert` 
-                from
-                    `wahlschein`
-                    
-            ) `q` on(
-                `q`.`kombiniert` = `wahlschein`.`kombiniert`
+            `bwkiel`.`wahlschein`
+            join `bwkiel`.`stimmzettel` on(
+                `bwkiel`.`wahlschein`.`stimmzettel` = `bwkiel`.`stimmzettel`.`ridx`
             )
+            join wahlberechtigte
+            	on wahlschein.wahlberechtigte = wahlberechtigte.ridx
+            join wahlberechtigte_anlage
+            on wahlberechtigte.identnummer = wahlberechtigte_anlage.identnummer
+            
+
         )
-        -- left join `wm_wahlschein_register` `w` on(`w`.`id` = `q`.`id`)
+        join (
+            select
+                `bwkiel`.`wahlschein`.`id` AS `id`,
+                `bwkiel`.`wahlschein`.`pwhash` AS `pwhash`,
+                `bwkiel`.`wahlschein`.`username` AS `username`,
+                `bwkiel`.`wahlschein`.`kombiniert` AS `kombiniert`
+            from
+                `bwkiel`.`wahlschein`
+        ) `q` on(
+            `q`.`kombiniert` = `bwkiel`.`wahlschein`.`kombiniert`
+        )
     )
-group by `q`.`username`
+group by
+    `q`.`username`
