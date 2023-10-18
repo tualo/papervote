e541949f4d72a22b14baff856834ecb9d604fb2d
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-18 09:35:42 +0200

    fix bilder

--- src/sql/install/kandidaten_bilder_typen.sql
+++ src/sql/install/kandidaten_bilder_typen.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS `kandidaten_bilder_typen` (
   `id` varchar(12) NOT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ;
