53e642f24044e0333ee7cf53d24ba8470ee09aa9
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-18 22:32:34 +0200

    allow local

--- src/Routes/SetupHandshake.php
+++ src/Routes/SetupHandshake.php
@@ -28,13 +28,43 @@ class SetupHandshake implements IRoute{
                 
                 $ping_result = false;
                 if ($_REQUEST['domain'] == 'localhost'){
-                    $ping_result = [
-                        'success' => true
-                    ];
-                }
+                   
 
-                if (
-                    $ping_result = $ping_result || APIRequestHelper::query( $_REQUEST['uri'].'/~/'.$_REQUEST['token'].'/onlinevote/ping',[] )
+                    $keys = TualoApplicationPGP::keyGen(2048);
+                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)",[
+                        'system_settings_id'    => 'erp/privatekey',
+                        'property'              => $keys['private']
+                    ]);
+    
+                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)",[
+                        'system_settings_id'    => 'remote-erp/public',
+                        'property'              => $_REQUEST['publickey']
+                    ]);
+
+                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)",[
+                        'system_settings_id'    => 'remote-erp/url',
+                        'property'              => $_REQUEST['uri']
+                    ]);
+
+                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)",[
+                        'system_settings_id'    => 'remote-erp/token',
+                        'property'              => $_REQUEST['token']
+                    ]);
+    
+                    $token = $session->registerOAuth(
+                            $params     =   [],
+                            $force      =   true,
+                            $anyclient  =   false,
+                            $path       =   '/papervote/*'
+                    );
+                    $session->oauthValidDays($token,365);
+                    
+                    App::result('publickey', $keys['public']);
+                    App::result('token', $token);
+                    App::result('message_public', TualoApplicationPGP::enarmor(TualoApplicationPGP::encrypt($_REQUEST['publickey'],$token)));
+                    App::result('success', true );
+                }else if (
+                    $ping_result = APIRequestHelper::query( $_REQUEST['uri'].'/~/'.$_REQUEST['token'].'/onlinevote/ping',[] )
                 ){
                     if (
                         ($ping_result==false)||
