10a9bf45e3a304c9e5366e6c814e5d13af3de52b
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-11-21 12:40:07 +0100

    shorten code

--- src/js/both/commands/PWGenPWCommand.js
+++ src/js/both/commands/PWGenPWCommand.js
@@ -88,155 +88,50 @@ Ext.define('Tualo.PaperVote.commands.WMPWGenPWCommand', {
     me.wahlschein = o.wahlschein;
     me.username = o.username;
     me.password = o.password;
-
-    await me.gatteringData(0);
-    await me.loopPWHashRange(0);
-    me.saveExcel();
+    me.current = 0;
+    me.blocksize = 250;
+    await me.loopPWRange();
+    
+    
     return true;
 
   },
-  gatteringData: function(){
-    let me = this,
-      range = me.records,
-      index = 0;
-    return new Promise((resolve) => {
-      me.loopPWRange(0, resolve);
-//      resolve();
-    })
-  },
+  
 
-  loopPWRange: function (index, cb) {
+  loopPWRange: async function () {
     let me = this,
         range = me.records,
         i=0,
         progressbar_data = me.getComponent('form').getComponent('progressbar_data');
       
-        if (index < range.length) {
+        if (me.current < range.length) {
           range[0].store.suspendEvents(true);
-          while(i<250 && index < range.length){
-            range[index].set('password',me.password[index].val);
-            range[index].set('wahlscheinnummer',me.wahlschein[index].val);
-            range[index].set('username',me.username[index].val);
-            index++;
+          while(i<me.blocksize && index < range.length){
+            range[me.current].set('password',me.password[me.current].val);
+            range[me.current].set('wahlscheinnummer',me.wahlschein[me.current].val);
+            range[me.current].set('username',me.username[me.current].val);
+            me.current++;
             i++;
           }
 
           range[0].store.resumeEvents();
-          progressbar_data.updateProgress((index + 1) / range.length);
+          progressbar_data.updateProgress((me.current + 1) / range.length);
 
-          setTimeout(function () {
-            //c onsole.log('setTimeout',index);
-            me.loopPWRange(index, cb);
-          }, 1);
+          await me.loopPWHashRange();
+          await me.loopPWRange();
         } else {
-          cb();
-          //me.saveExcel();
         }
-        index++;
   },
 
-  /*
-  gatteringData: function () {
-    let me = this;
-    return new Promise((resolve) => {
-      me.loopPWRange(0, resolve);
-    })
-  },
-
-  loopPWRange: function (index, cb) {
-    let me = this,
-      progressbar_unique = me.getComponent('form').getComponent('progressbar_unique'),
-      progressbar_data = me.getComponent('form').getComponent('progressbar_data'),
-      progressbar_save = me.getComponent('form').getComponent('progressbar_save'),
-      ws = '',
-      idx = 0,
-      range = me.records;
-
-    console.log(index, range.length);
-
-    if (index < range.length) {
-
-
-
-      ws = me.plainPassword(8, me.passwordChars);
-      range[index].set('password', ws);
-
-      ws = me.plainPassword(8, me.wahlscheinChars);
-      while (typeof me.wahlschein[ws] != 'undefined') {
-        ws = me.plainPassword(8, me.wahlscheinChars);
-        idx++;
-        if (idx > 10000) throw Error('Zu lange');
-      }
-      range[index].set('wahlscheinnummer', ws);
-      me.wahlschein[ws] = ws; idx = 0;
-
-      ws = me.plainPassword(8, me.usernameChars);
-      while (typeof me.username[ws] != 'undefined') {
-        ws = me.plainPassword(8, me.usernameChars);
-        idx++;
-        if (idx > 10000) throw Error('Zu lange');
-      }
-      range[index].set('username', ws);
-      me.username[ws] = ws; idx = 0;
-
-
-      progressbar_data.updateProgress((index + 1) / range.length);
-
-      setTimeout(function () {
-        //c onsole.log('setTimeout',index);
-        me.loopPWRange(++index, cb);
-      }, 1);
-
-    } else {
-      cb();
-      //me.saveExcel();
-    }
-  },
-  passwordChars: 'ABCDEFGHJKLMNPRSTUVXYZabcdefghijkmpstuvxyz123456789',
-  usernameChars: 'ABCDEFGHJKLMNPRSTUVXYZabcdefghijkmpstuvxyz123456789',
-  wahlscheinChars: '1234567890',
-  */
-  saveExcel: function () {
-    let me = this;
-
-
-    let btncfg = {
-      type: 'excel07',
-      ext: 'xlsx',
-      includeGroups: true,
-      includeSummary: true
-    };
-
-    var cfg = Ext.merge({
-      title: 'PWGen Datenexport',
-      fileName: 'Datenexport' + '.' + (btncfg.ext || btncfg.type)
-    }, btncfg);
-    me.list.saveDocumentAs(cfg);
-
-
-
-  },
-
-  singleSync: function(){
-    let me = this,
-      range = me.records,
-      index = 0;
-    return new Promise((resolve) => {
-      range.store.sync({
-        callback: resolve
-      });
-    })
-  },
-
-  loopPWHashRange: async function (index) {
+  loopPWHashRange: async function () {
     let me = this,
       block_size = 250,
       progressbar_save = me.getComponent('form').getComponent('progressbar_save')
       ;
     try {
       while (index < me.records.length) {
-        progressbar_save.updateProgress((index + 1) / me.records.length);
-        let pw_list = me.records.slice(index, index + block_size);
+        progressbar_save.updateProgress((me.current + 1) / me.records.length);
+        let pw_list = me.store.getModifiedRecords();;
         let pws_list = pw_list.map((item) => {
           return {
             id: item.get('__id'),
@@ -262,8 +157,6 @@ Ext.define('Tualo.PaperVote.commands.WMPWGenPWCommand', {
         });
 
         await me.singleSync();
-
-        index += block_size;
       }
     } catch (e) {
       console.log(e);
@@ -276,108 +169,42 @@ Ext.define('Tualo.PaperVote.commands.WMPWGenPWCommand', {
       });
     }
 
-    progressbar_save.updateProgress(1);
+    //progressbar_save.updateProgress(1);
   },
 
-  loopSaveRange: async function (index) {
-    let me = this,
-      progressbar_unique = me.getComponent('form').getComponent('progressbar_unique'),
-      progressbar_data = me.getComponent('form').getComponent('progressbar_data'),
-      progressbar_save = me.getComponent('form').getComponent('progressbar_save'),
-      ws = '',
-      idx = 0,
-      range = me.saving;
-    //c onsole.log(index,range.length);
-
-    if (index < range.length) {
+  saveExcel: function () {
+    let me = this;
 
 
-      progressbar_save.updateProgress((index + 1) / range.length);
-      try {
-        let pw_list = range.slice
-        let r = await (await fetch('./pwgen/bcrypt', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            password: range[index].password
-          })
-        })).json();
+    let btncfg = {
+      type: 'excel07',
+      ext: 'xlsx',
+      includeGroups: true,
+      includeSummary: true
+    };
 
-        range[index].pwhash = r.pwhash;
-        me.loopSaveRange(++index);
-      } catch (e) {
-        console.log(e);
-        Ext.toast({
-          html: "Es ist ein Fehler aufgetreten: ",
-          title: 'Fehler',
-          width: 400,
-          align: 't',
-          iconCls: 'fa fa-warning'
-        });
-      }
-      /*
-      Tualo.Ajax.request({
-          //showWait: true,
-          timeout: 10*60000,
-          url: './pwgen/setpw',
-          params: range[index],
-          json: function(o){
-              if (o.success){
-                setTimeout(function(){
-                  me.loopSaveRange(++index);
-              },1);
-                
-              }else{
-                Ext.toast({
-                  html: o.msg,
-                  title: 'Fehler',
-                  width: 400,
-                  align: 't',
-                  iconCls: 'fa fa-warning'
-               });
-              }
-            }
-          });
-      */
+    var cfg = Ext.merge({
+      title: 'PWGen Datenexport',
+      fileName: 'Datenexport' + '.' + (btncfg.ext || btncfg.type)
+    }, btncfg);
+    me.list.saveDocumentAs(cfg);
 
-    } else {
-      me.fireEvent('completed');
-    }
+
+
   },
 
-
-  plainPassword: function (length, allowed) {
-    var result = '';
-    var allowedChars = allowed.split('');
-    var min = 0;
-    var max = allowedChars.length - 1;
-    var cnt = 0;
-    while (result.length < length) {
-      result += allowedChars[Math.round(Math.random() * (max - min) + min)];
-    }
-    return result;
+  singleSync: function(){
+    let me = this;
+    return new Promise((resolve) => {
+      range.store.sync({
+        callback: resolve
+      });
+    })
   },
 
   getNextText: function () {
     return 'Erzeugen';
   },
 
-  /*
-  buttons: [
-    {
-      text: 'Schliessen',
-      handler: function (btn) {
-        btn.up('pwgen_pw_command').fireEvent('cancled');
-      }
-    },
-    {
-      text: 'Verarbeiten',
-      handler: function (btn) {
-        var me = btn.up('pwgen_pw_command');
-        me.run(me.list);
-      }
-    }
-  ]*/
+
 });
