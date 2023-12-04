6711d380fdec7d3db9e92e3e4aaaf845f547facf
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-12-04 10:47:42 +0100

    md5

--- src/Checks/StoredProcedures.php
+++ src/Checks/StoredProcedures.php
@@ -14,10 +14,10 @@ class StoredProcedures extends PostCheck {
         $clientdb = App::get('clientDB');
         if (is_null($clientdb)) return;
         $def = [
-            'createRandomList'=>'3c1a32b3abe38f0766fc492abdf90e11',
-            'randomString'=>'3c1a32b3abe38f0766fc492abdf90e11',
-            'canChangeValue'=>'b2e2b2b2b2b2b2b2b2b2b2b2b2b2b2b2',
-            'getBallotpaper'=>'wfwef'
+            'createRandomList'=>'cbad8a5173ea90b446e3061b788dd5f8',
+            'randomString'=>'c327855664249e78a33f0370a38e4981',
+            'canChangeValue'=>'26dd4e31c0ca9e056cddb7f27102fb85',
+            'getBallotpaper'=>'2637d349dcbaa0307cdb28f7cb8e0652'
         ];
         self::procedureCheck(
             'ds',
