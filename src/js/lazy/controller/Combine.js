fd684d33251b6143efdddc1f09f67a24a3a063b4
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-12-06 09:24:23 +0100

    add nav

--- src/js/lazy/controller/Combine.js
+++ src/js/lazy/controller/Combine.js
@@ -8,5 +8,25 @@ Ext.define('Tualo.PaperVote.lazy.controller.Combine', {
         console.log('onInitialIdentChange',field, newValue, oldValue, eOpts);
         let res =await fetch('/papervote/identnummer/'+newValue).then( (response) => response.json() );
         console.log(res);
+    },
+
+    showNext: function () {
+        this.doCardNavigation(1);
+    },
+
+    showPrevious: function (btn) {
+        this.doCardNavigation(-1);
+    },
+
+    doCardNavigation: function (incr) {
+        let me = this,
+            l = me.getLayout(),
+            i = l.activeItem.id.split('card-')[1],
+            next = parseInt(i, 10) + incr,
+            vm = me.getViewModel();
+
+        l.setActiveItem(next);
+        vm.set('currentCardIndex', next );
+        
     }
 });
