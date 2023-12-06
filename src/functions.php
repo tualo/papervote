5a8ccefb42bad7b88358a88a380fddb068bf5e61
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-12-06 13:39:13 +0100

    add route

--- src/functions.php
+++ src/functions.php
@@ -28,6 +28,7 @@ require_once "Routes/counting/Save.php";
 
 require_once "Routes/combine/Save.php";
 require_once "Routes/combine/ReverseCheck.php";
+require_once "Routes/combine/Reset.php";
 
 require_once "Routes/pwgen/CreateColumns.php";
 require_once "Routes/pwgen/DS.php";
