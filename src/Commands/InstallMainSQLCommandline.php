35263b87b9d74f0d68b6fe99f8d69a0f9425e802
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-04 21:21:50 +0100

    add view_beteiligung_stimmzettel

--- src/Commands/InstallMainSQLCommandline.php
+++ src/Commands/InstallMainSQLCommandline.php
@@ -72,6 +72,8 @@ class InstallMainSQLCommandline implements ICommandline{
             'stimmzettelgruppen.ds' => 'setup stimmzettelgruppen.ds ',
 
 
+            'view_beteiligung_stimmzettel' => 'setup view_beteiligung_stimmzettel ',
+            //'view_beteiligung_stimmzettel.ds' => 'setup view_beteiligung_stimmzettel.ds ',
 
             'kandidaten' => 'setup kandidaten ',
             'kandidaten.preview' => 'setup kandidaten.preview ',
