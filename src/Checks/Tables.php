7c295f8faf60f1bf11e08a0b7ecb6e668ea89434
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-04 21:30:37 +0100

    add check

--- src/Checks/Tables.php
+++ src/Checks/Tables.php
@@ -69,6 +69,7 @@ class Tables  extends PostCheck {
             'view_blocksystem_status_blocks'=>[],
             'view_blocksystem_status_blocks_wahlscheine'=>[],
             'view_double_voter'=>[],
+            'view_beteiligung_stimmzettel'=>[],
         ];
         self::tableCheck('papervote',$tables,
             "please run the following command: `./tm install-sql-papervote --client ".$clientdb->dbname."`",
