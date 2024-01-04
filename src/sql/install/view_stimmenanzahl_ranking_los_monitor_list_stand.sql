5f3c029fdab943077e3f02b6cc511d7dcf9b2635
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-04 14:58:52 +0100

    shorten pugs fix view_stimmenanzahl_ranking_los_monitor_list_stand

--- src/sql/install/view_stimmenanzahl_ranking_los_monitor_list_stand.sql
+++ src/sql/install/view_stimmenanzahl_ranking_los_monitor_list_stand.sql
@@ -1,2 +1,3 @@
 DELIMITER ;
-create  or replace view  `view_stimmenanzahl_ranking_los_monitor_list_stand` as select 1 AS `mx`,1 AS `heute`;
+create or replace view view_stimmenanzahl_ranking_los_monitor_list_stand as
+select concat('Stand: ' , date_format( CONVERT_TZ( ifnull(max(mx),now()) ,'+00:00','+01:00') ,'%d. %M %Y %H:%i'))  mx from view_stimmenanzahl_ranking_los_monitor_list_gescannt;
