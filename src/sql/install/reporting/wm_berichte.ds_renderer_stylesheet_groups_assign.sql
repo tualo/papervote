cf96f22165020db3a3b2f62df8d2e2b3f695300c
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-05 08:06:38 +0100

    add ds rendere;

--- src/sql/install/reporting/wm_berichte.ds_renderer_stylesheet_groups_assign.sql
+++ src/sql/install/reporting/wm_berichte.ds_renderer_stylesheet_groups_assign.sql
@@ -1,3 +1,4 @@
+delimiter ;
 insert ignore into ds_renderer_stylesheet_groups_assign (group_id,pug_id,active) values ('1','wm_berichte',1);
 insert ignore into ds_renderer_stylesheet_groups_assign (group_id,pug_id,active) values ('2','wm_berichte',1);
 insert ignore into ds_renderer_stylesheet_groups_assign (group_id,pug_id,active) values ('3','wm_berichte',1);
@@ -14,3 +15,14 @@ insert ignore into ds_renderer_stylesheet_groups_assign (group_id,pug_id,active)
 insert ignore into ds_renderer_stylesheet_groups_assign (group_id,pug_id,active) values ('2','wm_berichte_tanboegen',1);
 insert ignore into ds_renderer_stylesheet_groups_assign (group_id,pug_id,active) values ('3','wm_berichte_tanboegen',1);
 insert ignore into ds_renderer_stylesheet_groups_assign (group_id,pug_id,active) values ('4','wm_berichte_tanboegen',1);
+
+insert ignore into ds_renderer (
+    table_name,
+    pug_template,
+    label
+)
+values (
+    'view_beteiligung_stimmzettel_pro_tag',
+    'wm_berichte_beteiligung_tagesprokoll_wrapper',
+    'PDF'
+);
