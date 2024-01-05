0276a1346c33aafc4b029a7116d0c68332d17889
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-05 09:36:29 +0100

    fix

--- src/js/lazy/controller/Input.js
+++ src/js/lazy/controller/Input.js
@@ -136,11 +136,11 @@ Ext.define('Tualo.PaperVote.lazy.controller.Input', {
 
     this.logic.on('saved', function (o, msg, object) {
       console.log('saved', object); 
-      window.x = me.Logic.getSaveText().join(', ')
+      window.x = me.logic.getSaveText().join(', ')
       window.y = me;
-      
+
       me.savedMask = Ext.toast({
-        html: me.Logic.getSaveText().join(', ') + ' ' + ' um ' + (Ext.util.Format.date(new Date(), 'H:i\'s')),
+        html: me.logic.getSaveText().join(', ') + ' ' + ' um ' + (Ext.util.Format.date(new Date(), 'H:i\'s')),
         title: 'Gespeichert',
         width: 300,
         align: 't',
