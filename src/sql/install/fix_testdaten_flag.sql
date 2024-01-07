9f0a6a2a0f8ec750f295cada33edf697aba7256b
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-07 09:28:57 +0100

    fix testdaten

--- src/sql/install/fix_testdaten_flag.sql
+++ src/sql/install/fix_testdaten_flag.sql
@@ -6,7 +6,7 @@ create or replace procedure `fix_testdaten_flag`() begin
     for rec in (
         select concat(stimmzettel.id,lpad(wahlberechtigte_anlage.identnummer,8,'0')) x from wahlberechtigte_anlage  join stimmzettel on wahlberechtigte_anlage.stimmzettel = stimmzettel.ridx where testdaten=1
     ) do
-        set @sql = concat('update wahlschein set testdaten=1 where id=',rec.x,' where testdaten=0');
+        set @sql = concat('update wahlschein set testdaten=1 where id=',rec.x,' and testdaten=0');
         prepare stmt from @sql;
         execute stmt;
         deallocate prepare stmt;
