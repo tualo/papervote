c371cc3ef1091c3540ee7471a446329a5cca5349
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-07 09:31:45 +0100

    check for fix_testdaten_flag

--- src/Checks/StoredProcedures.php
+++ src/Checks/StoredProcedures.php
@@ -17,7 +17,8 @@ class StoredProcedures extends PostCheck {
             'createRandomList'=>'cbad8a5173ea90b446e3061b788dd5f8',
             'randomString'=>'c327855664249e78a33f0370a38e4981',
             'canChangeValue'=>'26dd4e31c0ca9e056cddb7f27102fb85',
-            'getBallotpaper'=>'2637d349dcbaa0307cdb28f7cb8e0652'
+            'getBallotpaper'=>'2637d349dcbaa0307cdb28f7cb8e0652',
+            'fix_testdaten_flag'=>'08e596c9b49dc1f9ff03d29c4ec28089'
         ];
         self::procedureCheck(
             'ds',
