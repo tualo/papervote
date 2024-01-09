67098c8d3006d8dba4beb6443b90034e2988d774
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-09 18:51:06 +0100

    add data

--- src/sql/install/wm_berichte.data.sql
+++ src/sql/install/wm_berichte.data.sql
@@ -26,3 +26,19 @@ insert ignore into wm_berichte (id,name) values ('wm_berichte_zaehlprotokoll','Z
 insert ignore into wm_berichte (id,name) values ('wm_erwartet','Protokoll Erwartet');
 
 
+
+INSERT INTO `wm_berichte` VALUES
+('wm_bekanntmachung','Bekanntmachung des Wahlausschusses - Wahlergebnis'),
+('wm_berichte_abgewiesene_online','Protokolle Abgewiesene Online-Wähler'),
+('wm_berichte_abschlussbericht_gesamt','Abschlussbericht - Gesamt'),
+('wm_berichte_beteiligung_gesamt','Gesamtbeteiligung'),
+('wm_berichte_beteiligung_tagesprokoll','Beteiligung Tagesprokoll'),
+('wm_berichte_blocksystem','Blocksystem'),
+('wm_berichte_double_voter','Doppelwähler'),
+('wm_berichte_losentscheid_stimmzettel','Losentscheide nach Stimmzettel'),
+('wm_berichte_los_mindestsitze','Losentscheide nach Mindestsitz'),
+('wm_berichte_offlinewahl_teilergebnis','Teilergebnis der Briefwahl'),
+('wm_berichte_onlinewahl_teilergebnis','Teilergebnis der Onlinewahl'),
+('wm_berichte_prozentuale_verteilung','Erreichte Stimmenanzahl je Kandidaten'),
+('wm_berichte_zaehlprotokoll','Zählprotokoll'),
+('wm_erwartet','Protokoll Erwartet');
