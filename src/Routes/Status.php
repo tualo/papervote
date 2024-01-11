4b0ed1f9b3dd2799b082df98c98181e297e1b9de
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-11 20:05:39 +0100

    test state

--- src/Routes/Status.php
+++ src/Routes/Status.php
@@ -5,6 +5,7 @@ namespace Tualo\Office\PaperVote\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
+use Tualo\Office\PaperVote\APIRequestHelper;
 
 class Status implements IRoute
 {
@@ -14,6 +15,21 @@ class Status implements IRoute
         BasicRoute::add('/papervote/status',function($matches){
             App::contenttype('application/json');
             App::result('basistyp',App::configuration('papervote','basistyp','stimmzettel'));
+            
+            // /onlinevote/state
+            $db = App::get('session')->getDB();
+            $o = $db->directMap("
+                select if(property<>'',1,0) v,'api' text FROM system_settings WHERE system_settings_id = 'remote-erp/url' 
+                union 
+                select property v,'api_url' text FROM system_settings WHERE system_settings_id = 'remote-erp/url'
+                union
+                select property v,'api_token' text FROM system_settings WHERE system_settings_id = 'remote-erp/token'
+                union
+                select property v,'api_private' text FROM system_settings WHERE system_settings_id = 'erp/privatekey'
+            ",[],'text','v');
+            
+            $ping_result = APIRequestHelper::query( $o['api_url'].'/~/'.$o['api_token'].'/onlinevote/state',[] );
+            App::result('state',$ping_result);
             App::result('success',true);
         }, ['get'], true);
     }
