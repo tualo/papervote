b315030ca16ca2d65bb10b3a65e25b709a588a7d
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-17 19:30:28 +0100

    flatfile and view_stimmenanzahl_ranking_los_monitor_list_stand

--- src/sql/install/view_stimmenanzahl_ranking_los_monitor_list_stand.sql
+++ src/sql/install/view_stimmenanzahl_ranking_los_monitor_list_stand.sql
@@ -1,3 +1,2 @@
 DELIMITER ;
-create or replace view view_stimmenanzahl_ranking_los_monitor_list_stand as
-select concat('Stand: ' , date_format( ifnull(max(mx),now()) ,'%d. %M %Y %H:%i'))  mx from view_stimmenanzahl_ranking_los_monitor_list_gescannt;
+create or replace view view_stimmenanzahl_ranking_los_monitor_list_stand as select concat('Stand: ' , date_format( ifnull(max(mx),now()) ,'%d. %M %Y %H:%i'))  mx from view_stimmenanzahl_ranking_los_monitor_list_gescannt;
