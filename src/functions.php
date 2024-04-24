feb3d576c86c1cb7dae5201156bfcb51de14a150
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-04-24 12:43:13 +0200

    include route

--- src/functions.php
+++ src/functions.php
@@ -24,6 +24,7 @@ require_once "Routes/BCrypt.php";
 require_once "Routes/Sounds.php";
 require_once "Routes/Status.php";
 
+require_once "Routes/optical/Save.php";
 
 require_once "Routes/counting/SkipStartBallotPaper.php";
 require_once "Routes/counting/Save.php";
