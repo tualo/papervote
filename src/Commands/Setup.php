4756583900b0189acc6a0e803535bca54ac847e2
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2024-11-13 13:58:41 +0100

    fix setup

--- src/Commands/Setup.php
+++ src/Commands/Setup.php
@@ -8,8 +8,9 @@ use Tualo\Office\ExtJSCompiler\Helper;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\PostCheck;
 use Tualo\Office\Basic\CommandLineInstallSessionSQL;
+use Tualo\Office\Basic\BaseSetupCommand as BaseSetup;
 
-class Setup implements ISetupCommandline{
+class Setup extends BaseSetup implements ISetupCommandline{
 
     public static function getCommandName(): string { return 'papervote'; }
     public static function getCommandDescription(): string { return 'perform a complete papervote setup'; }
@@ -25,5 +26,47 @@ class Setup implements ISetupCommandline{
         PostCheck::formatPrintLn(['blue'], "Installing all needed SQL procedures for papervote module");
         PostCheck::formatPrintLn(['blue'], "==========================================================");
         
+
+        /*
+        exec(implode(' ',['which','scss']),$result,$return_code);
+        $sass_cmd = $result[0];
+
+        exec(implode(' ',['which','sencha']),$result,$return_code);
+        $sencha_cmd = $result[0];
+
+
+        $installCommands = [
+            'configuration --section scss --key cmd --value '.$sass_cmd,
+            'configuration --section ext-compiler --key sencha_compiler_command --value '. $sencha_cmd,
+            // 'configuration --section ext-compiler --key requires --value "exporter"',
+        ];
+        foreach($installCommands as $cmdString){
+            self::performInstall($cmdString,'');
+        }
+        */
+
+        $installCommands = [
+            'install-sessionsql-bsc-main',
+            'install-sql-sessionviews',
+            'install-sql-bsc-main-ds',
+            'install-sql-bsc-menu',
+            'install-sql-ds-main',
+            'install-sql-ds',
+            'install-sql-ds-dsx',
+            'install-sql-ds-privacy',
+            'install-sql-ds-docsystem',
+            'install-sql-tualojs',
+            'install-sql-monaco',
+            'install-sql-dashboard',
+            'install-sql-bootstrap',
+            'install-sql-papervote-menu',
+            'install-sql-papervote'
+            // 'compile'
+        ];
+ 
+        foreach($installCommands as $cmdString){
+            self::performInstall($cmdString,$clientName);
+        }
+
     }
 }
