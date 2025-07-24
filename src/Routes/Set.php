a44feead4db39cfa06fa58fbefc4259f83860a96
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-24 08:27:02 +0200

    fix composer.json and VERSION file, update Set and SetupHandshake routes

--- src/Routes/Set.php
+++ src/Routes/Set.php
@@ -16,68 +16,102 @@ class Set implements IRoute
     public static function register()
     {
 
-        BasicRoute::add('/papervote/set', function () {
-            App::contenttype('application/json');
-            $db = App::get('session')->getDB();
-            try {
+        BasicRoute::add(
+            '/papervote/set',
+            function () {
+                App::contenttype('application/json');
+                $db = App::get('session')->getDB();
+                try {
 
 
-                $db->direct('start transaction;');
-                $remote_public_key = $db->singleValue("select property FROM system_settings WHERE system_settings_id = 'remote-erp/public'", [], 'property');
-                if (!isset($_REQUEST['voter_id'])) throw new Exception("voter_id missed");
-                if (!isset($_REQUEST['signature']))  throw new Exception("signature missed");
-                if (!isset($_REQUEST['ballotpaper_id']))  throw new Exception("ballotpaper_id missed");
-                if (!TualoApplicationPGP::verify($remote_public_key, $_REQUEST['voter_id'], $_REQUEST['signature'])) throw new Exception("Verification failed");
-
-                if (ctype_digit($_REQUEST['voter_id']) === false) throw new Exception("voter_id is not a number");
-                if (ctype_digit($_REQUEST['ballotpaper_id']) === false) throw new Exception("ballotpaper_id is not a number");
+                    $db->direct('start transaction;');
+                    $remote_public_key = $db->singleValue("select property FROM system_settings WHERE system_settings_id = 'remote-erp/public'", [], 'property');
+                    if (!isset($_REQUEST['voter_id'])) throw new Exception("voter_id missed");
+                    if (!isset($_REQUEST['signature']))  throw new Exception("signature missed");
+                    if (!isset($_REQUEST['ballotpaper_id']))  throw new Exception("ballotpaper_id missed");
 
 
-                $sql = 'select * from wahlschein where id={voter_id} and stimmzettel  = {stimmzettel} and (wahlscheinstatus,abgabetyp) in (select wahlscheinstatus,abgabetyp from wahlscheinstatus_online_erlaubt)';
-                $data = $db->singleRow($sql, [
-                    'voter_id'      =>  $_REQUEST['voter_id'],
-                    'stimmzettel'   =>  $_REQUEST['ballotpaper_id'],
-                ]);
 
-                if ($data !== false) {
-                    $db->direct("update wahlschein set wahlscheinstatus=2, abgabetyp=2, update_date=curdate(), update_time=curtime() where id={voter_id}  and stimmzettel={stimmzettel} and (wahlscheinstatus,abgabetyp) in (select wahlscheinstatus,abgabetyp from wahlscheinstatus_online_erlaubt) ", [
-                        'voter_id'      => $_REQUEST['voter_id'],
-                        'stimmzettel'   => $_REQUEST['ballotpaper_id'],
-                    ]);
+                    if (!TualoApplicationPGP::verify($remote_public_key, $_REQUEST['voter_id'], $_REQUEST['signature'])) throw new Exception("Verification failed");
 
-                    $sql = 'select * from wahlschein where id={voter_id} and stimmzettel  = {stimmzettel} and wahlscheinstatus=2 and abgabetyp=2';
+                    if (ctype_digit($_REQUEST['voter_id']) === false) throw new Exception("voter_id is not a number");
+                    if (ctype_digit($_REQUEST['ballotpaper_id']) === false) throw new Exception("ballotpaper_id is not a number");
+
+
+                    $sql = 'select * from wahlschein where id={voter_id} and stimmzettel  = {stimmzettel} and (wahlscheinstatus,abgabetyp) in (select wahlscheinstatus,abgabetyp from wahlscheinstatus_online_erlaubt)';
                     $data = $db->singleRow($sql, [
                         'voter_id'      =>  $_REQUEST['voter_id'],
-                        'stimmzettel'   => $_REQUEST['ballotpaper_id'],
+                        'stimmzettel'   =>  $_REQUEST['ballotpaper_id'],
                     ]);
-                    if ($data !== false) App::result('success', true);
 
-                    try {
-                        $formdata = $_REQUEST;
-                        unset($formdata['signature']);
-                        unset($formdata['voter_id']);
-                        unset($formdata['ballotpaper_id']);
-                        // alter table wm_wahlschein_register add formdata json default null
-                        $sql = 'update wm_wahlschein_register set formdata={formdata} where id={voter_id} and stimmzettel={stimmzettel}';
-                        $db->direct($sql, [
+                    if ($data !== false) {
+                        $db->direct("update wahlschein set wahlscheinstatus=2, abgabetyp=2, update_date=curdate(), update_time=curtime() where id={voter_id}  and stimmzettel={stimmzettel} and (wahlscheinstatus,abgabetyp) in (select wahlscheinstatus,abgabetyp from wahlscheinstatus_online_erlaubt) ", [
                             'voter_id'      => $_REQUEST['voter_id'],
                             'stimmzettel'   => $_REQUEST['ballotpaper_id'],
-                            'formdata'      => json_encode($formdata)
                         ]);
-                    } catch (\Exception $e) {
-                    }
-                    $db->direct('commit;');
-                } else {
 
+                        $sql = 'select * from wahlschein where id={voter_id} and stimmzettel  = {stimmzettel} and wahlscheinstatus=2 and abgabetyp=2';
+                        $data = $db->singleRow($sql, [
+                            'voter_id'      =>  $_REQUEST['voter_id'],
+                            'stimmzettel'   => $_REQUEST['ballotpaper_id'],
+                        ]);
+                        if ($data !== false) App::result('success', true);
+
+                        try {
+                            $formdata = $_REQUEST;
+                            unset($formdata['signature']);
+                            unset($formdata['voter_id']);
+                            unset($formdata['ballotpaper_id']);
+                            // alter table wm_wahlschein_register add formdata json default null
+                            $sql = 'update wm_wahlschein_register set formdata={formdata} where id={voter_id} and stimmzettel={stimmzettel}';
+                            $db->direct($sql, [
+                                'voter_id'      => $_REQUEST['voter_id'],
+                                'stimmzettel'   => $_REQUEST['ballotpaper_id'],
+                                'formdata'      => json_encode($formdata)
+                            ]);
+                        } catch (\Exception $e) {
+                        }
+                        $db->direct('commit;');
+                    } else {
+
+                        $db->direct('rollback;');
+                        App::result('success', false);
+                        App::result('msg', ($data === false) ? 'Konnte nicht gespeichert werden' : '');
+                    }
+                } catch (\Exception $e) {
                     $db->direct('rollback;');
-                    App::result('success', false);
-                    App::result('msg', ($data === false) ? 'Konnte nicht gespeichert werden' : '');
+                    App::result('last_sql', $db->last_sql);
+                    App::result('msg', $e->getMessage());
                 }
-            } catch (\Exception $e) {
-                $db->direct('rollback;');
-                App::result('last_sql', $db->last_sql);
-                App::result('msg', $e->getMessage());
-            }
-        }, ['post'], true);
+            },
+            ['post'],
+            true,
+
+            [
+                'errorOnUnexpected' => false,
+                'errorOnInvalid' => false,
+                'fields' => [
+                    'ballotpaper_id' => [
+                        'required' => false,
+                        'type' => 'int',
+                        'min' => 0,
+                        'max' => 10000000
+                    ],
+                    'voter_id' => [
+                        'required' => true,
+                        'type' => 'int',
+                        'min' => 0,
+                        'max' => 10000000
+                    ],
+                    'signature' => [
+                        'required' => true,
+                        'type' => 'string',
+                        'min' => 0,
+                        'max' => 1000000
+                    ],
+                ]
+
+            ]
+        );
     }
 }
--- src/Routes/SetupHandshake.php
+++ src/Routes/SetupHandshake.php
@@ -16,66 +16,23 @@ class SetupHandshake implements IRoute
     public static function register()
     {
 
-        BasicRoute::add('/papervote/setuphandshake', function ($matches) {
-            try {
-                $session = App::get('session');
-                $db = $session->getDB();
-                App::contenttype('application/json');
+        BasicRoute::add(
+            '/papervote/setuphandshake',
+            function ($matches) {
+                try {
+                    $session = App::get('session');
+                    $db = $session->getDB();
+                    App::contenttype('application/json');
 
 
-                if (!isset($_REQUEST['publickey'])) throw new \Exception("missing parameter");
-                if (!isset($_REQUEST['uri'])) throw new \Exception("missing parameter");
-                if (!isset($_REQUEST['token'])) throw new \Exception("missing parameter");
-                if (!isset($_REQUEST['message'])) throw new \Exception("missing parameter");
+                    if (!isset($_REQUEST['publickey'])) throw new \Exception("missing parameter");
+                    if (!isset($_REQUEST['uri'])) throw new \Exception("missing parameter");
+                    if (!isset($_REQUEST['token'])) throw new \Exception("missing parameter");
+                    if (!isset($_REQUEST['message'])) throw new \Exception("missing parameter");
 
 
-                $ping_result = false;
-                if ($_REQUEST['domain'] == 'localhost') {
-
-
-                    $keys = TualoApplicationPGP::keyGen(2048);
-                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
-                        'system_settings_id'    => 'erp/privatekey',
-                        'property'              => $keys['private']
-                    ]);
-
-                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
-                        'system_settings_id'    => 'remote-erp/public',
-                        'property'              => $_REQUEST['publickey']
-                    ]);
-
-                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
-                        'system_settings_id'    => 'remote-erp/url',
-                        'property'              => $_REQUEST['uri']
-                    ]);
-
-                    $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
-                        'system_settings_id'    => 'remote-erp/token',
-                        'property'              => $_REQUEST['token']
-                    ]);
-
-                    $token = $session->registerOAuth(
-                        $params     =   [],
-                        $force      =   true,
-                        $anyclient  =   false,
-                        $path       =   '/papervate/*'
-                    );
-                    $session->oauthValidDays($token, 365);
-
-                    App::result('publickey', $keys['public']);
-                    App::result('token', $token);
-                    App::result('message_public', TualoApplicationPGP::enarmor(TualoApplicationPGP::encrypt($_REQUEST['publickey'], $token)));
-                    App::result('success', true);
-                } else if (
-                    $ping_result = APIRequestHelper::query($_REQUEST['uri'] . '/~/' . $_REQUEST['token'] . '/onlinevote/ping', [])
-                ) {
-                    if (
-                        ($ping_result == false) ||
-                        (!isset($ping_result['success'])) ||
-                        ($ping_result['success'] !== true)
-                    ) {
-                        throw new \Exception("Das Onlinewahlsystem kann nicht angepingt werden.");
-                    } else {
+                    $ping_result = false;
+                    if ($_REQUEST['domain'] == 'localhost') {
 
 
                         $keys = TualoApplicationPGP::keyGen(2048);
@@ -103,7 +60,7 @@ class SetupHandshake implements IRoute
                             $params     =   [],
                             $force      =   true,
                             $anyclient  =   false,
-                            $path       =   '/papervote/*'
+                            $path       =   '/papervate/*'
                         );
                         $session->oauthValidDays($token, 365);
 
@@ -111,15 +68,102 @@ class SetupHandshake implements IRoute
                         App::result('token', $token);
                         App::result('message_public', TualoApplicationPGP::enarmor(TualoApplicationPGP::encrypt($_REQUEST['publickey'], $token)));
                         App::result('success', true);
-                    }
-                } else {
-                    throw new \Exception("Das Onlinewahlsystem kann nicht angepingt werden. (2) ");
-                }
-            } catch (\Exception $e) {
+                    } else if (
+                        $ping_result = APIRequestHelper::query($_REQUEST['uri'] . '/~/' . $_REQUEST['token'] . '/onlinevote/ping', [])
+                    ) {
+                        if (
+                            ($ping_result == false) ||
+                            (!isset($ping_result['success'])) ||
+                            ($ping_result['success'] !== true)
+                        ) {
+                            throw new \Exception("Das Onlinewahlsystem kann nicht angepingt werden.");
+                        } else {
 
-                App::result('last_sql', $db->last_sql);
-                App::result('msg', $e->getMessage());
-            }
-        }, ['post', 'get'], true);
+
+                            $keys = TualoApplicationPGP::keyGen(2048);
+                            $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
+                                'system_settings_id'    => 'erp/privatekey',
+                                'property'              => $keys['private']
+                            ]);
+
+                            $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
+                                'system_settings_id'    => 'remote-erp/public',
+                                'property'              => $_REQUEST['publickey']
+                            ]);
+
+                            $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
+                                'system_settings_id'    => 'remote-erp/url',
+                                'property'              => $_REQUEST['uri']
+                            ]);
+
+                            $db->direct("insert into system_settings (system_settings_id,property) values ({system_settings_id},{property}) on duplicate key update property=values(property)", [
+                                'system_settings_id'    => 'remote-erp/token',
+                                'property'              => $_REQUEST['token']
+                            ]);
+
+                            $token = $session->registerOAuth(
+                                $params     =   [],
+                                $force      =   true,
+                                $anyclient  =   false,
+                                $path       =   '/papervote/*'
+                            );
+                            $session->oauthValidDays($token, 365);
+
+                            App::result('publickey', $keys['public']);
+                            App::result('token', $token);
+                            App::result('message_public', TualoApplicationPGP::enarmor(TualoApplicationPGP::encrypt($_REQUEST['publickey'], $token)));
+                            App::result('success', true);
+                        }
+                    } else {
+                        throw new \Exception("Das Onlinewahlsystem kann nicht angepingt werden. (2) ");
+                    }
+                } catch (\Exception $e) {
+
+                    App::result('last_sql', $db->last_sql);
+                    App::result('msg', $e->getMessage());
+                }
+            },
+            ['post', 'get'],
+            true,
+            [
+                'errorOnUnexpected' => false,
+                'errorOnInvalid' => false,
+                'fields' => [
+
+                    'token' => [
+                        'required' => true,
+                        'type' => 'string',
+                        'min' => 0,
+                        'max' => 1000000
+                    ],
+                    'publickey' => [
+                        'required' => true,
+                        'type' => 'string',
+                        'min' => 0,
+                        'max' => 1000000
+                    ],
+                    'uri' => [
+                        'required' => true,
+                        'type' => 'string',
+                        'min' => 0,
+                        'max' => 1000000
+                    ],
+                    'message' => [
+                        'required' => true,
+                        'type' => 'string',
+                        'min' => 0,
+                        'max' => 1000000
+                    ],
+                    'domain' => [
+                        'required' => true,
+                        'type' => 'string',
+                        'min' => 0,
+                        'max' => 1000000
+                    ],
+
+                ]
+
+            ]
+        );
     }
 }
