aceaeb32e31f74294b7cbb03fbade25a98bc070d
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-01-15 08:57:20 +0100

    Version bump to 2.2.42

--- src/Checks/StoredProcedures.php
+++ src/Checks/StoredProcedures.php
@@ -7,25 +7,26 @@ use Tualo\Office\Basic\PostCheck;
 use Tualo\Office\Basic\TualoApplication as App;
 
 
-class StoredProcedures extends PostCheck {
+class StoredProcedures extends PostCheck
+{
 
-    
-    public static function test(array $config){
+
+    public static function test(array $config)
+    {
         $clientdb = App::get('clientDB');
         if (is_null($clientdb)) return;
         $def = [
-            'createRandomList'=>'cbad8a5173ea90b446e3061b788dd5f8',
-            'randomString'=>'c327855664249e78a33f0370a38e4981',
-            'canChangeValue'=>'26dd4e31c0ca9e056cddb7f27102fb85',
-            'getBallotpaper'=>'2637d349dcbaa0307cdb28f7cb8e0652',
-            'fix_testdaten_flag'=>'84445815fa5e69e6d746ac5a1840d940'
+            'createRandomList' => 'cbad8a5173ea90b446e3061b788dd5f8',
+            'randomString' => 'c327855664249e78a33f0370a38e4981',
+            'canChangeValue' => '26dd4e31c0ca9e056cddb7f27102fb85',
+            'getBallotpaper' => '2637d349dcbaa0307cdb28f7cb8e0652',
+            // 'fix_testdaten_flag'=>'84445815fa5e69e6d746ac5a1840d940'
         ];
         self::procedureCheck(
             'ds',
             $def,
-            "please run the following command: `./tm install-sql-papervote --client ".$clientdb->dbname."`",
-            "please run the following command: `./tm install-sql-papervote --client ".$clientdb->dbname."`"
+            "please run the following command: `./tm install-sql-papervote --client " . $clientdb->dbname . "`",
+            "please run the following command: `./tm install-sql-papervote --client " . $clientdb->dbname . "`"
         );
-        
     }
 }
